a5b14943f4a211ef013fb08e1d48a686252ab74d
Ant00000ny <Ant00000ny@icloud.com>
2024-02-23 15:54:44 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+    id("maven-publish")
+}
+
+group = "com.isekaiofficial"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        mavenLocal()
+    }
+}
